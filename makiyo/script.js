5afb012e3f9acf128ab10997673a07efc574a7b1
makiyo-ronkko <56912851+makiyo-ronkko@users.noreply.github.com>
2020-02-19 14:22:37 +0200

    navigation bar responsive & pop-up window

--- makiyo/script.js
+++ makiyo/script.js
@@ -1,7 +1,8 @@
+/* Navigation bar */
 // Selection of HTML objects
 var burger = document.querySelector('.burger i');
 var nav = document.querySelector('.nav');
-// Defining a function
+
 function toggleNav() {
     burger.classList.toggle('fa-bars');
     burger.classList.toggle('fa-times');
@@ -10,4 +11,40 @@ function toggleNav() {
 // Calling the function after click event occurs
 burger.addEventListener('click', function () {
     toggleNav();
+    document.querySelector('.nav-active').style.display = 'block';
 });
+
+/* Pop-up window on Project card */
+
+var modal = document.getElementById('pop-up');
+var btn = document.getElementById('projectBtn');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        this.modal.style.display = 'none';
+    }
+}
+
+/* Scroll top button */
+$(document).ready(function () {
+    var pagetop = $('#page_top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //appear after 100px scroll down
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+});
+
+
