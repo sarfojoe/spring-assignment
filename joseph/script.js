4288cd17df2b46aace4349b9b75f1d245f6d99a2
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-02-19 16:12:38 +0200

    updated

--- joseph/script.js
+++ joseph/script.js
@@ -1,3 +1,12 @@
-$('.carousel').carousel({
-    interval: 2000
-  })
+var img = document.getElementById('img')
+var images = ['africa1.webp', 'africa2.webp','africa3.webp','sunset2.webp','sunset1.jpg']
+var x = 0;
+function slide (){
+    if (x<images.length){
+        x = x+1;
+    }else{
+        x = 1;
+    }
+    img.innerHTML = "<img src="+images[x-1]+">";
+}
+setInterval(slide, 3000);
