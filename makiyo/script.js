3b43ff8d3f18b6de45772a62d3747474db702d8f
makiyo-ronkko <56912851+makiyo-ronkko@users.noreply.github.com>
2020-02-20 17:44:22 +0200

    modals & hero banner animation

--- makiyo/script.js
+++ makiyo/script.js
@@ -13,26 +13,91 @@ burger.addEventListener('click', function () {
     toggleNav();
 });
 
-s
-/* Pop-up window on Project card */
+/* Pop-up window on Skills card */
 
-var modal = document.getElementById('pop-up');
-var btn = document.getElementById('projectBtn');
-var span = document.getElementsByClassName('close')[0];
+function skillsPopUp() {
+    var btn = document.getElementById('skillsBtn');
+    var modal = document.getElementById('skills-pop-up');
+    var span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function () {
-    modal.style.display = 'block';
-}
-
-span.onclick = function () {
-    modal.style.display = 'none';
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        this.modal.style.display = 'none';
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            this.modal.style.display = 'none';
+        }
     }
 }
+skillsPopUp();
+
+/* Pop-up window on Workexperience card */
+
+function workPopUp() {
+    var modal = document.getElementById('work-pop-up');
+    var btn = document.getElementById('workBtn');
+    var span = document.getElementsByClassName('close')[1];
+
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            this.modal.style.display = 'none';
+        }
+    }
+}
+workPopUp();
+
+
+/* Pop-up window on Education card */
+
+function educationPopUp() {
+    var modal = document.getElementById('education-pop-up');
+    var btn = document.getElementById('educationBtn');
+    var span = document.getElementsByClassName('close')[2];
+
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            this.modal.style.display = 'none';
+        }
+    }
+}
+educationPopUp();
+
+
+/* Pop-up window on Project card */
+
+function projectPopUp() {
+    var modal = document.getElementById('project-pop-up');
+    var btn = document.getElementById('projectBtn');
+    var span = document.getElementsByClassName('close')[3];
+
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            this.modal.style.display = 'none';
+        }
+    }
+}
+projectPopUp();
 
 /* Scroll top button */
 $(document).ready(function () {
