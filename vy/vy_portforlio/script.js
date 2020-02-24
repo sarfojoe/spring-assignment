08260123d0c7ba30514ab1f740e29dbd76910d97
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-02-24 18:59:52 +0200

    random

--- vy/vy_portforlio/script.js
+++ vy/vy_portforlio/script.js
@@ -52,12 +52,13 @@ $(document).ready(function() {
 // slick slider
 $(document).ready(function() {
   $(".slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
     centerMode: true,
-    variableWidth: true
+    variableWidth: true,
+    arrow: true
   });
 });
 
