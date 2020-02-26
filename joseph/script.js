1374c6599fd5f34e07fe8ea1329bf22175ad3619
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-02-26 23:06:42 +0200

    updated

---
+++ joseph/script.js
@@ -0,0 +1,34 @@
+// Smooth scrolling
+$('#navbar a, .btn').on('click', function(event){
+    if(this.hash !=='') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -80
+            },
+            800
+        );
+    }
+
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
