ad88f158122bc6471530e08e2bc235246009ab21
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-02-27 19:17:51 +0200

    js- scroll-btn

--- joseph/script.js
+++ joseph/script.js
@@ -15,3 +15,20 @@ $('#navbar a, .btn').on('click', function(event){
 
 });
 
+jQuery(document).ready(function () {
+
+    var btn = $('#button');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+})    
