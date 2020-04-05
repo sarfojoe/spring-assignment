366545cc2af4f19acdc4733fbb3349e8d12335cc
bea94k <59803918+bea94k@users.noreply.github.com>
2020-04-05 12:02:37 +0300

    Intermediate pull request (#16)
    * Skill cards, contact - CV download button
    
    Added:
    - Fontawesome .js
    - icons and flags to skill cards
    - progress bars to skill cards
    - fonts
    - CV download button
    - button styling
    - minor changes to the footer.
    
    * Logo, nav bar working, mailto, contact buttons
    
    Added:
    - logo (basic one)
    - nav bar links working
    - smooth scrolling
    - mailto in contact page and in navbar
    - buttons in contact section: email, LinkedIn, GitHub, making them work + styling
    - minor changes in styling the page.
    
    * Slick slider part 1
    
    - added slider with random images
    - tried to center the images, playing with height and width
    - added slider folder with necessary files
    
    * Small changes in slider
    
    - images in slider centered vertically
    - prepared to merge branches dev1 and dev2
    
    * Edu and exp cards filled, progress bars corrected, img slider kind of responsive
    
    - edu and exp cards formatted
    - edu cards filled with text
    - exp cards filled with placeholders
    - progress bars put on correct level
    - image slider responsive - stage 1, difference for big screens and others
    - minor changes to styling and comments in code
    
    * My images instead of placeholders, filled texts in edu and exp
    
    Added:
    - my pictures in the slider and in the contact page, alts + temporary styling to flip the vertical ones
    - real texts in education and experience cards
    - minor styling
    Deleted:
    - two placeholder photos (1 left)
    
    * Intro text on landing page, back to start button, color scheme, automatic slider, vertical scrolling read as horizontal scrolling
    
    Added:
    - pictures in education and experience sections instead of placeholders
    - font to use in intro, to match the logo
    - introduction text on the homepage
    - back to start button: styling + working
    - colors changed to proper working color scheme
    - image slider moving automatically
    - reading vertical mouse scroll as horizontal scrolling
    - minor styling changes
    
    Deleted:
    - placeholder picture
    
    * Fake load animation, AOS animations, navbar hover effects
    
    Added:
    - fake loader for a few seconds - timeouts, "hide" class, credits for the creator etc.
    - AOS animations on the landing page
    - navbar buttons color change on hoover
    - an arrow on the home page hinting to the user which way the scrolling will go
    - minor changes to styling
    - formatting of the code (Prettier finally working on my laptop).
    
    * Favicon, weather API, readme.md
    
    Added:
    - favicon files and link
    - weather API used to get Helsinki's current temperature, displayed in the footer
    - readme file added with the description of the project
    
    * Google Analytics + disclaimer, hover effect on exp and edu cards
    
    Added:
    - Google Analytics andr on the fake loading screen a disclaimer about the use of cookies on the website
    - hover effect on education and experience cards - displaying the description only after hovering on the card
    -minor changes in styling.

---
+++ beata/README.md
@@ -0,0 +1,26 @@
+# My portfolio page
+
+This project is my personal portfolio website. It is a horizontal website built mainly with JavaScript, HTML, CSS and external libraries. It shows my skills, education, experience and serves as my web developer CV.
+
+## Built With
+
+- HTML5, CSS3
+- JavaScript
+- jQuery
+- [Font Awesome](https://fontawesome.com/) - icons
+- [Slick Slider](https://kenwheeler.github.io/slick/) - image slider by Ken Wheeler
+- [Favicon.io](https://favicon.io/) - favicon generator
+- [Simple loader](https://codepen.io/I-is-kevin/details/jqejXj) - loader by Kevin on codepen.io
+- [AOS](https://michalsnik.github.io/aos/) - Animate On Scroll library by michalsnik
+- [OpenWeather API](https://michalsnik.github.io/aos/)
+
+## Authors
+
+- **Beata Kuśnierz** - [bea94k](https://github.com/bea94k)
+
+## Acknowledgments
+
+- Ken Wheeler for the slick slider
+- Kevin for the loading animation
+- michalsnik for AOS library
+- Business College Helsinki, especially our teachers Margit Tennosaar and Hoang Pham
--- beata/js/main.js
+++ beata/js/main.js
@@ -1,5 +1,196 @@
-function sayHi() {
-    console.log('henlo der')
-}
+jQuery(document).ready(function() {
+  /* CV button opens a pdf file with an option of downloading the file */
+  $(".cv-btn").click(function() {
+    window.open(
+      "https://drive.google.com/file/d/1jB85BXs7fTBjAALDJ_OYJXcQTonPAX_I/view?usp=sharing"
+    );
+  });
 
-sayHi()
+  /* SLIDER - depending on the width of the user's screen when the page is loaded, the slider has different properties (doesn't work when resizing the window - only with resize + refresh page) */
+  //timeout to wait till the fake loading animation is done
+  setTimeout(function() {
+    if ($(window).width() > "840") {
+      /* when user's screen is wider than 840px, do this */
+      $(".slider").slick({
+        dots: true,
+        wariableWidth: true,
+        wariableHeight: true,
+        autoplay: true
+      });
+    } else {
+      $(".slider").slick({
+        dots: true,
+        arrows: false,
+        wariableWidth: true,
+        wariableHeight: true,
+        autoplay: true
+      });
+    }
+  }, 2500);
+
+  /* BACK TO TOP BUTTON - appearing after a scroll from left */
+
+  var topBtn = $(".to-top-btn");
+
+  if ($(".scrolling-wrapper").scrollLeft() <= 1000) {
+    topBtn.css("display", "none");
+  }
+
+  $(".scrolling-wrapper").scroll(function() {
+    if ($(".scrolling-wrapper").scrollLeft() > 1000) {
+      topBtn.fadeIn(500);
+    } else {
+      topBtn.fadeOut(500);
+    }
+  });
+
+  topBtn.on("click", function(event) {
+    event.preventDefault();
+    $(".scrolling-wrapper").animate({ scrollLeft: 0 }, "1000");
+  });
+
+  /* HANDLING SCROLLING VERTICALLY AS SCROLLING HORIZONTALLY 
+  (arrows left and right work after clicking anywhere in the scrolling-wrapper)*/
+
+  window.addEventListener("wheel", function(e) {
+    /* might not be supported in IE!! */
+    var moreScrollRight = $(".scrolling-wrapper").scrollLeft() + 350;
+    var moreScrollLeft = $(".scrolling-wrapper").scrollLeft() - 350;
+
+    if (e.deltaY < 0) {
+      $(".scrolling-wrapper").animate({ scrollLeft: moreScrollLeft }, 0);
+    }
+    if (e.deltaY > 0) {
+      $(".scrolling-wrapper").animate({ scrollLeft: moreScrollRight }, 0);
+    }
+  });
+
+  /* FAKE LOADING OF THE PAGE */
+  // add spinner to beginning of body of each html
+  $("body").append(
+    '<div class="loader"><div class="line"></div><div class="line"></div><div class="line"></div><div class="line"></div><div id="disclaimer">This site uses Google Analytics to improve the usability and ensure the best experience on the site. By continuing to use this site you accept the use of cookies.</div></div> <!-- "simple loader", a pen by Kevin on https://codepen.io/I-is-kevin/details/jqejXj -->'
+  );
+  $("body").css("background", "#212922");
+
+  // side-wrap (the whole website) add class "hide", so that the setTimeout works with the spinner: show spinner for 2.5 sec, then hide but show website
+  $(".site-wrap").addClass("hide");
+
+  setTimeout(function() {
+    $(".loader").addClass("hide");
+    $(".site-wrap").removeClass("hide");
+  }, 2500);
+
+  /* AOS animations */
+  // append link and script of the AOS animations to the head of each html
+  $("head")
+    .append(
+      '<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />'
+    )
+    .append('<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>');
+  // append initialization to the end of body of each html; timeout because fake loading for 2.5 sec
+  $("body").append(
+    "<script>setTimeout(function(){ AOS.init(); }, 2500);</script>"
+  );
+
+  // add data-aos to all elements I want to be animated
+  // homepage
+  $(".line2, .line3, .line4, .line5, .line6, .line7").addClass("transparent");
+  //making the lines transparent keeps the lines on a specific screen height on the landing page (no jumping when appearing where there was nothing - this div is with display: flex). Removing the transparent ones and inserting the visible ones with animation whenever I want them to appear with setTimeout.
+
+  $(".line1")
+    .attr("data-aos", "zoom-in")
+    .attr("data-aos-duration", "1500");
+
+  /*  line 2 */
+  setTimeout(function() {
+    $(".line2").remove();
+    $('<p class="line2">New web development projects.</p>').insertAfter(
+      ".line1"
+    );
+    $(".line2")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 4000);
+
+  /* line 3 */
+  setTimeout(function() {
+    $(".line3").remove();
+    $('<p class="line3">New opportunities in the IT field.</p>').insertAfter(
+      ".line2"
+    );
+    $(".line3")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 5500);
+
+  /* line 4 */
+  setTimeout(function() {
+    $(".line4").remove();
+    $('<p class="line4">New ways to go.</p>').insertAfter(".line3");
+    $(".line4")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 7000);
+
+  /*  line 5 */
+  setTimeout(function() {
+    $(".line5").remove();
+    $('<p class="line5">New adventures.</p>').insertAfter(".line4");
+    $(".line5")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 8500);
+
+  /* line 6 */
+  setTimeout(function() {
+    $(".line6").remove();
+    $('<p class="line6">New travel destinations.</p>').insertAfter(".line5");
+    $(".line6")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 10000);
+
+  /* line 7 */
+  setTimeout(function() {
+    $(".line7").remove();
+    $("<h1 class='line7'>I'm in.</h1>").insertAfter(".line6");
+    $(".line7")
+      .attr("data-aos", "zoom-in")
+      .attr("data-aos-duration", "1500");
+  }, 11500);
+
+  /*  The single line animations probably could be done in a cleaner way, but couldn't figure it out for now, with so many of the parameters, classes and all changing case to case. */
+
+  // current temperature in Hesinki with Weather API in the footer, rounded up to 0.1
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?q=helsinki&units=metric&appid=5fa0b75646bc8623b0656789ee77d610"
+  }).done(function(resp) {
+    var curTemp = Math.round(resp.main.temp * 10) / 10;
+    $(".temp").text(`Current temp in Helsinki, FI: ${curTemp} deg C`);
+  });
+
+  //  EDUCATION AND EXPERIENCE CARDS ANIMATION ON HOVER
+  //  original state: pictures visible, description hidden
+
+  $(".edu-exp-descr").addClass("hide");
+
+  // after hovering: picture hidden, description visible
+
+  $("#high-school").mouseenter(expandCard);
+  $("#uni").mouseenter(expandCard);
+  $("#bch").mouseenter(expandCard);
+  $("#lionbridge").mouseenter(expandCard);
+  $("#nhl").mouseenter(expandCard);
+  $("#fafas").mouseenter(expandCard);
+
+  function expandCard() {
+    // this here refers to what was hovered on, so the specific card
+    var cardId = $(this).attr("id");
+    $(this).css("max-height", "95%");
+    $(`#${cardId} .edu-exp-img`).addClass("hide");
+    $(`#${cardId} .edu-exp-descr`).css("display", "block");
+  }
+
+  //  fadein/out
+});
